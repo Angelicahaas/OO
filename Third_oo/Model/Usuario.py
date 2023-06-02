9ccf7ead0e2e9c27159f0065c5fd73fb3b8be038
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2023-06-02 18:21:49 -0300

    Create Usuario.py

---
+++ Third_oo/Model/Usuario.py
@@ -0,0 +1,10 @@
+
+class Usuario():
+    
+    def __init__(self, nome: str, endereco: str, telefone: int, senha: str):
+        self.__nome = nome
+        self.__endereco = endereco
+        self.__teledone = telefone
+        self.__senha = senha
+
+
